7f3ea13f002b654d5a8dcc751864fe906761ad08
Leonid <brebiv@gmail.com>
2020-05-28 11:06:25 +0300

    Fixed replies
    There was missing space

--- bot/utils/localization/lang/russian.py
+++ bot/utils/localization/lang/russian.py
@@ -6,4 +6,4 @@ class RussianText:
     main_menu_text = f"{Emoji.victory_hand} Привет, это бот в котором вы можете рисовать\n Попробуй!"
     you_already_have_drawing_link_text = "Используйте эту ссылку: "
     default_link_text = "Ссылка"
-    click_to_draw = "Перейдите по ссылке"
+    click_to_draw = "Перейдите по ссылке "
