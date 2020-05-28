3739654770a58116703ed962d0d4424cbe14aa43
Leonid <brebiv@gmail.com>
2020-05-28 10:41:28 +0300

    Added config and database

---
+++ config.py
@@ -0,0 +1,13 @@
+class BotConfig:
+
+    API_TOKEN = "Your token here"
+
+
+class WebServerConfig:
+
+    ADDRESS = "http://localhost:5000"
+
+
+class DataBaseConfig:
+
+    url = "sqlite:///db.sqlite3"
